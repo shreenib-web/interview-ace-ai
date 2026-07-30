188baae38960f4422780a9d546afba4fff4b0404
Shreeni <shreeni.b@z-aksys.com>
2026-07-30 13:03:23 +0530

    Add application routing, Create authentication pages and Create public pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-bold text-cyan-400">
-        InterviewAce AI 🚀
-      </h1>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/public/Home";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Dashboard from "../pages/dashboard/Dashboard";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
